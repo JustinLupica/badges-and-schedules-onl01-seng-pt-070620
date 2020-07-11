07bea3ebec7b99de5d52b07e30727ab2abcd6902
Justin Lupica <justinlupica@gmail.com>
2020-07-11 19:47:50 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,11 @@
 # Write your code here.
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees.each do |attendee|
+  return "Hello, my name is #{attendee}."
+end
+end
